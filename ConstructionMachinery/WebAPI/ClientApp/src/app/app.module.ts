6a0481740031d32a130ab227e56ae6dcdcc1e734
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-08-17 17:34:11 +0300

    Создал первые страницы
    Создал главную страницу, авторизации и регистрации

--- ConstructionMachinery/WebAPI/ClientApp/src/app/app.module.ts
+++ ConstructionMachinery/WebAPI/ClientApp/src/app/app.module.ts
@@ -1,32 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './pages/main/main.component';
+import { AuthoriseComponent } from './pages/authorise/authorise.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    AuthoriseComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    FormsModule,
+    BrowserModule,
+    AppRoutingModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+      { path: '', component: MainComponent },
+      { path: 'authorise', component: AuthoriseComponent },
+      { path: 'registration', component: RegistrationComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ConstructionMachinery/WebAPI/ClientApp/src/app/pages/authorise/authorise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authorise',
+  templateUrl: './authorise.component.html',
+  styleUrls: ['./authorise.component.css']
+})
+export class AuthoriseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

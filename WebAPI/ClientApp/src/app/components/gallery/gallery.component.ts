d14bc5b2680724a6e74caf8af1d91d07a801905a
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-09-23 17:14:51 +0300

    Изменил стили, добавил модификаторы

--- WebAPI/ClientApp/src/app/components/gallery/gallery.component.ts
+++ WebAPI/ClientApp/src/app/components/gallery/gallery.component.ts
@@ -9,18 +9,28 @@ import { ImageModel } from 'src/app/models/ImageModel';
 export class GalleryComponent implements OnInit {
 
   @Input() images: ImageModel[] = [];
+  @Input() dotCount: number = 0;
+  public dotArray: boolean[] = []
   public index: number = 0;
 
   public nexImage(): void {
+    this.dotArray[this.index] = false;
     this.index++;
     if(this.index == this.images.length) {
       this.index = 0;
     }
+    this.dotArray[this.index] = true;
   }
 
   constructor() { }
 
-  ngOnInit(): void {
+  public async ngOnInit(): Promise<void> {
+    for(let count = 0; count < this.dotCount; count++) {
+      await this.dotArray.push(false);
+    }
+    if(this.dotArray.length > 0) {
+      this.dotArray[0] = true;
+    }
   }
 
 }

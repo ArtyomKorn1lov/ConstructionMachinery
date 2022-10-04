f249474b7e2dd8ca58457a9990b6741b51686677
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2022-10-04 12:10:16 +0300

    Привёл код галереи к презентабельному виду

--- WebAPI/ClientApp/src/app/components/gallery/gallery.component.ts
+++ WebAPI/ClientApp/src/app/components/gallery/gallery.component.ts
@@ -1,21 +1,25 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ImageModel } from 'src/app/models/ImageModel';
 
+enum SwipeDirection {
+  Left = "LEFT",
+  Right = "RIGHT"
+};
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
-
   @Input() images: ImageModel[] = [];
-  public dotArray: boolean[] = []
+
+  private posX?: number = 0;
+
   public activeIndex: number = 0;
-  public posX?: number = 0;
 
   public nexImage(): void {
-    if (this.activeIndex === this.images.length - 1)
-    {
+    if (this.activeIndex === this.images.length - 1) {
       this.activeIndex = 0;
     } else {
       this.activeIndex++;
@@ -23,8 +27,7 @@ export class GalleryComponent implements OnInit {
   }
 
   public backImage(): void {
-    if (this.activeIndex === 0)
-    {
+    if (this.activeIndex === 0) {
       this.activeIndex = this.images.length - 1;
     } else {
       this.activeIndex--;
@@ -32,62 +35,67 @@ export class GalleryComponent implements OnInit {
   }
 
   public touchStart(event: TouchEvent): void {
-    this.posX = event.targetTouches.item(0)?.pageX;
+    if (this.images.length !== 1) {
+      this.posX = event.targetTouches.item(0)?.pageX;
+    }
   }
 
   public touchEnd(event: TouchEvent): void {
-    let x = event.changedTouches.item(0)?.pageX;
-    if(this.posX && x != null) {
-      if((x - this.posX) > 50) {
-        this.moveGallery("right");
-      }
-      if((x - this.posX) < -50) {
-        this.moveGallery("left");
+    if (this.images.length !== 1) {
+      let x = event.changedTouches.item(0)?.pageX;
+      if (this.posX && x != null) {
+        if (x - this.posX > 50) {
+          this.moveGallery(SwipeDirection.Right);
+        }
+        if (x - this.posX < -50) {
+          this.moveGallery(SwipeDirection.Left);
+        }
       }
     }
-      
   }
 
   public moveGallery(direction: string): void {
-    if(direction == "right") {
-      const images: HTMLCollectionOf<Element> = document.getElementsByClassName("gallery__image");
-      const nextIndex = this.activeIndex === 0 ? this.images.length - 1 : this.activeIndex - 1;
+    if (direction == SwipeDirection.Right) {
+      const images: HTMLCollectionOf<Element> =
+        document.getElementsByClassName('gallery__image');
+      const nextIndex =
+        this.activeIndex === 0 ? this.images.length - 1 : this.activeIndex - 1;
 
-      images[this.activeIndex].classList.add("-active");
-      images[nextIndex].classList.add("-active");
-      images[this.activeIndex].classList.add("-moving-right-from-center");
-      images[nextIndex].classList.add("-moving-right-from-left");
+      images[this.activeIndex].classList.add('-active');
+      images[nextIndex].classList.add('-active');
+      images[this.activeIndex].classList.add('-moving-right-from-center');
+      images[nextIndex].classList.add('-moving-right-from-left');
 
       setTimeout(() => {
-        images[this.activeIndex].classList.remove("-moving-right-from-center");
-        images[nextIndex].classList.remove("-moving-right-from-left");
+        images[this.activeIndex].classList.remove('-moving-right-from-center');
+        images[nextIndex].classList.remove('-moving-right-from-left');
 
-        images[this.activeIndex].classList.remove("-active");
+        images[this.activeIndex].classList.remove('-active');
         this.backImage();
-      }, 1000);
+      }, 300);
     }
-    if(direction == "left") {
-      const images: HTMLCollectionOf<Element> = document.getElementsByClassName("gallery__image");
-      const nextIndex = this.activeIndex === this.images.length - 1 ? 0 : this.activeIndex + 1;
+    if (direction == SwipeDirection.Left) {
+      const images: HTMLCollectionOf<Element> =
+        document.getElementsByClassName('gallery__image');
+      const nextIndex =
+        this.activeIndex === this.images.length - 1 ? 0 : this.activeIndex + 1;
 
-      images[this.activeIndex].classList.add("-active");
-      images[nextIndex].classList.add("-active");
-      images[this.activeIndex].classList.add("-moving-left-from-center");
-      images[nextIndex].classList.add("-moving-left-from-right");
+      images[this.activeIndex].classList.add('-active');
+      images[nextIndex].classList.add('-active');
+      images[this.activeIndex].classList.add('-moving-left-from-center');
+      images[nextIndex].classList.add('-moving-left-from-right');
 
       setTimeout(() => {
-        images[this.activeIndex].classList.remove("-moving-left-from-center");
-        images[nextIndex].classList.remove("-moving-left-from-right");
+        images[this.activeIndex].classList.remove('-moving-left-from-center');
+        images[nextIndex].classList.remove('-moving-left-from-right');
 
-        images[this.activeIndex].classList.remove("-active");
+        images[this.activeIndex].classList.remove('-active');
         this.nexImage();
-      }, 1000);
+      }, 300);
     }
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
